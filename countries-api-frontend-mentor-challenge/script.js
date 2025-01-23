b8f0494b1f3c59cc8d0f764ecca8c27aab0cfd7a
Abhijeet Gaur <abhijeetgaur.dev@gmail.com>
2025-01-24 00:24:06 +0530

    Api succesfully integrated

--- countries-api-frontend-mentor-challenge/script.js
+++ countries-api-frontend-mentor-challenge/script.js
@@ -1,8 +1,20 @@
+const heroSection = document.querySelector('.hero-section');
 fetch('https://restcountries.com/v3.1/all')
 .then((res)=>res.json())
 .then((data)=> {
     data.forEach((country) => {
-        console.log(country)
+        const countryCard = document.createElement('a');
+        countryCard.classList.add('country-card');
+        countryCard.innerHTML = `
+            <img src="${country.flags.svg}" alt="" class="country-flag">
+                    <div class="country-content">
+                        <h3>${country.name.common}</h3>
+                        <span><b>Population</b> : ${country.population.toLocaleString('en-IN')}</span>
+                        <span><b>Region</b> : ${country.region}</span>
+                        <span><b>Capital</b> : ${country.capital}</span>
+                    </div>
+        `
+        heroSection.append(countryCard);
     })
 
 })
