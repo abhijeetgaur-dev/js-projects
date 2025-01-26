5a91a82ca5b249d1cf57999d6509d40777189e1a
Abhijeet Gaur <abhijeetgaur.dev@gmail.com>
2025-01-26 23:00:18 +0530

    complete dynamic api implemenation

--- countries-api-frontend-mentor-challenge/country.js
+++ countries-api-frontend-mentor-challenge/country.js
@@ -1,36 +1,72 @@
 const countryName = new URLSearchParams(location.search).get('name');
-const countryBody = document.querySelector('.country-body');
+
+const countryFlag = document.querySelector(".country-flag");
+const countryTitle = document.querySelector(".country-title");
+const countryPopulation = document.querySelector(".country-population");
+const countryRegion = document.querySelector(".country-region");
+
 
 fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
 .then((res)=>res.json())
 .then((data)=> {
     const country = data[0];
-    console.log(Object.values(country.name.nativeName)[0])
+    console.log(Object.keys(country.currencies))
     console.log(country)
-    countryBody.innerHTML = `
-    <a class="back-button" href="#"><i class="fa-solid fa-arrow-left"></i> &nbsp;&nbsp;Back</a>
-            <div class="each-country-content">
-                    <img src="${country.flags.svg}" alt="${country.name.common}">
-                <div class="country-details">
-                    <h2>${country.name.common}</h2>
-                    <div class="country-content">
-                        <span><b>Native Name: </b>${Object.values(country.name.nativeName)[0].common}</span>
-                        <span><b>Population: </b>${country.population.toLocaleString("en-IN")}</span>
-                        <span><b>Region: </b> ${country.region}</span>
-                        <span><b>Sub-Region: </b> ${country.subregion}</span>
-                        <span><b>Capital: </b>${country.capital}</span>
-                        <span><b>Top Level Domain: </b>${country.tld[0]}</span>
-                        <span><b>Currencies: </b>${country.currencies}</span>
-                        <span><b>Languages: </b>${country.languages}</span>
-                    </div>
-                    <div class="border-countries">
-                        <b>Border Countries:</b>
-                        <div class="border-countries-content">
-                            <a href="#">Pakistan</a>
-                            <a href="#">Tajikistan</a>
-                            <a href="#">Uzbekistan</a>
-                        </div>
-                    </div>
-            </div>
-    `
+
+
+    countryFlag.src = country.flags.svg;
+    countryTitle.innerText = country.name.common;
+    if(country.name.nativeName){
+        const countryNativeName = document.querySelector(".country-nativeName");
+        countryNativeName.innerText = Object.keys(country.name.nativeName).join(", ");
+      }
+    countryPopulation.innerText = country.population.toLocaleString('en-IN');
+    countryRegion.innerText = country.region;
+
+    if(country.subregion){
+        const countrySubregion = document.querySelector(".country-subregion");
+        countrySubregion.innerText = country.subregion;
+    }
+
+    if(country.capital){
+        const countryCapital = document.querySelector(".country-capital");
+        countryCapital.innerText = country.capital.join(", ");
+    }
+
+    if(country.tld){
+        const countryTld = document.querySelector(".country-tld");
+        countryTld.innerText = country.tld.join(", ");
+    }
+
+    if(country.currencies){
+        const countryCurrencies = document.querySelector(".country-currencies");
+        countryCurrencies.innerText = Object.keys(country.currencies).join(", ");
+    }
+
+    if(country.languages){
+        const countryLanguages = document.querySelector(".country-languages");
+        countryLanguages.innerText = Object.keys(country.languages).join(", ");
+    }
+
+    if(country.borders){
+        const countryBorders = document.querySelector(".country-borders");
+        Object.values(country.borders).forEach((border) => {
+            console.log(border)
+            fetch(`https://restcountries.com/v3.1/alpha/${border}`).then(res=> res.json()).then(data=>{
+                console.log(data)
+                const borderCountries = document.querySelector(".border-countries-content");
+                const borderElement = document.createElement('a')
+                borderElement.innerText = data[0].name.common;
+                borderElement.href = "./country.html?name="+data[0].name.common;
+
+                borderCountries.append(borderElement);
+                
+            })
+            // const borderElement = document.createElement('a');
+            // borderElement.classList.add('border');
+            // borderElement.href = `./country.html?name=${border}`;
+            // borderElement.innerText = border;
+            // countryBorders.append(borderElement);
+        })
+    }
 })
--- countries-api-frontend-mentor-challenge/script.js
+++ countries-api-frontend-mentor-challenge/script.js
@@ -3,6 +3,7 @@ fetch('https://restcountries.com/v3.1/all')
 .then((res)=>res.json())
 .then((data)=> {
     data.forEach((country) => {
+        console.log(country.name.common + "{" +country.capital.join(", ")+"}")
         const countryCard = document.createElement('a');
         countryCard.classList.add('country-card');
         countryCard.href = `./country.html?name=${country.name.common}`;
